05c23c94baad95a8b78b7c9c2eff685ce8750220
이미연 <dreamcode0331@hivelab.co.kr>
2023-09-08 18:15:16 +0900

    [#1] 배너 슬라이드 마크업 진행중

--- seongnamsi/js/script.js
+++ seongnamsi/js/script.js
@@ -1,3 +1,4 @@
+// 통합검색 슬라이더
 let move = 0
 let speed = -1;
 const result = true; // result 값이 true일 때 이미지가 슬라이드 된다.
@@ -21,3 +22,23 @@ window.onload=function(){
   var cloneSlide = document.getElementById("slideList").cloneNode(true); //복제
   document.getElementById("slideBox").appendChild(cloneSlide);
 }
+
+// 메인배너 슬라이더
+const bannerSlide = new Swiper('.swiper.banner_slide', {
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+const btnPause = document.querySelector('.swiper-button-pause');
+const btnPlay = document.querySelector('.swiper-button-play');
+btnPause.addEventListener('click',() => console.log('멈춤', bannerSlide.autoplay.stop()));
+btnPlay.addEventListener('click',() => console.log('재생', bannerSlide.autoplay.start()));
