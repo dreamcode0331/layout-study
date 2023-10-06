0f4b9d15f25601bea7f61382bc4ad722835f35e9
이미연 <dreamcode0331@hivelab.co.kr>
2023-10-06 11:33:24 +0900

    메인 배너 슬라이드 버튼 수정

--- seongnamsi/js/script.js
+++ seongnamsi/js/script.js
@@ -1,9 +1,10 @@
 // 통합검색 슬라이더
 let move = 0
 let speed = -1;
-const result = true; // result 값이 true일 때 이미지가 슬라이드 된다.
+let result = true; // result 값이 true일 때 이미지가 슬라이드 된다.
 
 function motion(){
+  console.log('1111111111111111');
   if(result == true){
     move += speed;
     if(move <= -1000) {
@@ -25,20 +26,31 @@ window.onload=function(){
 
 // 메인배너 슬라이더
 const bannerSlide = new Swiper('.swiper.banner_slide', {
+  speed: 1000,
   autoplay: {
-    delay: 1000,
+    delay: 1500,
     disableOnInteraction: false,
   },
   loop: true,
   pagination: {
     el: '.swiper-pagination',
+    type: 'fraction',
   },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 });
+
+// 배너 슬라이드 재생/정지 버튼
 const btnPause = document.querySelector('.swiper-button-pause');
 const btnPlay = document.querySelector('.swiper-button-play');
 btnPause.addEventListener('click',() => console.log('멈춤', bannerSlide.autoplay.stop()));
 btnPlay.addEventListener('click',() => console.log('재생', bannerSlide.autoplay.start()));
+
+$('.banner_slide_btn.pause').on('click',function(){
+  $('.banner_slide_btn.pause').hide().siblings('.banner_slide_btn.play').show();
+})
+$('.banner_slide_btn.play').on('click',function(){
+  $('.banner_slide_btn.play').hide().siblings('.banner_slide_btn.pause').show();
+})
