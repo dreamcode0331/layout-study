dbedce6291d28cd93368749b6feb3807a1ce5e2e
이미연 <dreamcode0331@hivelab.co.kr>
2023-10-10 16:31:57 +0900

    section_service 마크업

--- seongnamsi/js/script.js
+++ seongnamsi/js/script.js
@@ -22,6 +22,8 @@ window.onload=function(){
   setInterval(motion, 30);
   var cloneSlide = document.getElementById("slideList").cloneNode(true); //복제
   document.getElementById("slideBox").appendChild(cloneSlide);
+
+  
 }
 
 // 메인배너 슬라이더
@@ -54,3 +56,24 @@ $('.banner_slide_btn.pause').on('click',function(){
 $('.banner_slide_btn.play').on('click',function(){
   $('.banner_slide_btn.play').hide().siblings('.banner_slide_btn.pause').show();
 })
+
+// 탭메뉴
+const tabServiceMenuBtn = document.querySelectorAll('.tab_service_menu_btn');
+const tabServiceContItem = document.querySelectorAll('.tab_service_cont_item');
+
+tabServiceMenuBtn.forEach((item, i) => {
+  item.addEventListener('click', () => {
+    tabServiceContItem.forEach((content) => {
+      content.classList.remove('is_on');
+    });
+
+    tabServiceMenuBtn.forEach((content) => {
+      content.classList.remove('is_on');
+    });
+
+    tabServiceMenuBtn[i].classList.add('is_on');
+    tabServiceContItem[i].classList.add('is_on');
+  })
+})
+
+
