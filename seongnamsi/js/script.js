05b76129cca79b733124fd495b3cace2d64a6c99
이미연 <dreamcode0331@hivelab.co.kr>
2023-10-13 17:56:36 +0900

    section board/event 영역 마크업

--- seongnamsi/js/script.js
+++ seongnamsi/js/script.js
@@ -26,9 +26,10 @@ window.onload=function(){
   
 }
 
-// 메인배너 슬라이더
-const bannerSlide = new Swiper('.swiper.banner_slide', {
-  speed: 1000,
+// 메인배너 슬라이드
+const bannerSlide = new Swiper('.banner_slide', {
+  focusableElements: 'input, select, option, textarea, video, label',
+  speed: 1200,
   autoplay: {
     delay: 1500,
     disableOnInteraction: false,
@@ -44,11 +45,53 @@ const bannerSlide = new Swiper('.swiper.banner_slide', {
   },
 });
 
-// 배너 슬라이드 재생/정지 버튼
+// 배너 슬라이드 (row)
+const bannerSlideRow = new Swiper('.banner_slide.row', {
+  focusableElements: 'input, select, option, textarea, video, label',
+  slidesPerView: 3,
+  spaceBetween: 25,
+  speed: 1500,
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+// 배너 슬라이드 (col)
+const bannerSlideCol = new Swiper('.banner_slide.col', {
+  focusableElements: 'input, select, option, textarea, video, label',
+  slidesPerView: 1.4,
+  spaceBetween: 20,
+  speed: 1800,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+// 배너 슬라이드 - 재생/정지 버튼
 const btnPause = document.querySelector('.swiper-button-pause');
 const btnPlay = document.querySelector('.swiper-button-play');
-btnPause.addEventListener('click',() => console.log('멈춤', bannerSlide.autoplay.stop()));
-btnPlay.addEventListener('click',() => console.log('재생', bannerSlide.autoplay.start()));
+btnPause.addEventListener('click',() => console.log('멈춤', bannerSlide.autoplay.stop(), bannerSlideRow.autoplay.stop(), bannerSlideCol.autoplay.stop()));
+btnPlay.addEventListener('click',() => console.log('재생', bannerSlide.autoplay.start(), bannerSlideRow.autoplay.start(), bannerSlideCol.autoplay.start()));
 
 $('.banner_slide_btn.pause').on('click',function(){
   $('.banner_slide_btn.pause').hide().siblings('.banner_slide_btn.play').show();
@@ -60,7 +103,7 @@ $('.banner_slide_btn.play').on('click',function(){
 // 탭메뉴 type2
 function tabType2(){
   const tabServiceMenuBtn = document.querySelectorAll('.tab_area.type2 .tab_menu_btn');
-  const tabServiceContItem = document.querySelectorAll('.tab_area.type2 .tab_cont_item');
+  const tabServiceContItem = document.querySelectorAll('.press_list');
 
   tabServiceMenuBtn.forEach((item, i) => {
     item.addEventListener('click', () => {
