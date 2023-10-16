f087e6699d1f6d280e484c973a147674db99d969
이미연 <dreamcode0331@hivelab.co.kr>
2023-10-16 17:53:35 +0900

    스와이퍼 슬라이드 재생/멈춤 버튼 수정

--- seongnamsi/js/script.js
+++ seongnamsi/js/script.js
@@ -1,40 +1,39 @@
 // 통합검색 슬라이더
-let move = 0
-let speed = -1;
-let result = true; // result 값이 true일 때 이미지가 슬라이드 된다.
+// let move = 0
+// let speed = -1;
+// let result = true;
 
-function motion(){
-  console.log('1111111111111111');
-  if(result == true){
-    move += speed;
-    if(move <= -1000) {
-      move = 0;
-    }
-    //오른쪽으로 슬라이드
-    if(move > 0){
-      move =- 1000;
-    }
-    document.getElementById("slideBox").style.left = move + "px";
-  }
-}
+// function motion(){
+//   console.log('1111111111111111');
+//   if(result == true){
+//     move += speed;
+//     if(move <= -1000) {
+//       move = 0;
+//     }
+//     if(move > 0) {
+//       move =- 1000;
+//     } 
+//     document.getElementById("slideBox").style.left = move + "px";
+//   }
+// }
 
-window.onload=function(){
-  setInterval(motion, 30);
-  var cloneSlide = document.getElementById("slideList").cloneNode(true); //복제
-  document.getElementById("slideBox").appendChild(cloneSlide);
+// window.onload=function(){
+//   setInterval(motion, 50);
+//   let cloneSlide = document.getElementById("slideList").cloneNode(true); //복제
+//   document.getElementById("slideBox").appendChild(cloneSlide);
+// }
 
-  
-}
 
 // 메인배너 슬라이드
 const bannerSlide = new Swiper('.banner_slide', {
   focusableElements: 'input, select, option, textarea, video, label',
-  speed: 1200,
-  autoplay: {
-    delay: 1500,
-    disableOnInteraction: false,
-  },
+  speed: 1800,
   loop: true,
+  loopAdditionalSlides : 1,
+  autoplay : {
+		delay : 3000,
+		disableOnInteraction : false,
+	},
   pagination: {
     el: '.swiper-pagination',
     type: 'fraction',
@@ -47,15 +46,16 @@ const bannerSlide = new Swiper('.banner_slide', {
 
 // 배너 슬라이드 (row)
 const bannerSlideRow = new Swiper('.banner_slide.row', {
-  focusableElements: 'input, select, option, textarea, video, label',
   slidesPerView: 3,
   spaceBetween: 25,
-  speed: 1500,
-  autoplay: {
-    delay: 1500,
-    disableOnInteraction: false,
-  },
+  autoplay:true,
+  speed: 1800,
   loop: true,
+  loopAdditionalSlides : 1,
+  autoplay : {
+		delay : 3000,
+		disableOnInteraction : false,
+	},
   pagination: {
     el: '.swiper-pagination',
     type: 'fraction',
@@ -68,16 +68,16 @@ const bannerSlideRow = new Swiper('.banner_slide.row', {
 
 // 배너 슬라이드 (col)
 const bannerSlideCol = new Swiper('.banner_slide.col', {
-  focusableElements: 'input, select, option, textarea, video, label',
   slidesPerView: 1.4,
   spaceBetween: 20,
+  autoplay:true,
   speed: 1800,
-  autoplay: {
-    delay: 2000,
-    disableOnInteraction: false,
-  },
   loop: true,
   loopAdditionalSlides : 1,
+  autoplay : {
+		delay : 3000,
+		disableOnInteraction : false,
+	},
   pagination: {
     el: '.swiper-pagination',
     type: 'fraction',
@@ -91,15 +91,16 @@ const bannerSlideCol = new Swiper('.banner_slide.col', {
 // 배너 슬라이드 - 재생/정지 버튼
 const btnPause = document.querySelector('.swiper-button-pause');
 const btnPlay = document.querySelector('.swiper-button-play');
-btnPause.addEventListener('click',() => console.log('멈춤', bannerSlide.autoplay.stop(), bannerSlideRow.autoplay.stop(), bannerSlideCol.autoplay.stop()));
-btnPlay.addEventListener('click',() => console.log('재생', bannerSlide.autoplay.start(), bannerSlideRow.autoplay.start(), bannerSlideCol.autoplay.start()));
-
-$('.banner_slide_btn.pause').on('click',function(){
-  $('.banner_slide_btn.pause').hide().siblings('.banner_slide_btn.play').show();
-})
-$('.banner_slide_btn.play').on('click',function(){
-  $('.banner_slide_btn.play').hide().siblings('.banner_slide_btn.pause').show();
-})
+btnPause.addEventListener('click', function(){
+  console.log('멈춤', bannerSlide.autoplay.stop(),bannerSlideRow.autoplay.stop(),bannerSlideCol.autoplay.stop());
+  btnPause.style.display = 'none';
+  btnPlay.style.display = 'block';
+});
+btnPlay.addEventListener('click', function(){
+  console.log('재생', bannerSlide.autoplay.start(),bannerSlideRow.autoplay.start(),bannerSlideCol.autoplay.start());
+  btnPlay.style.display = 'none';
+  btnPause.style.display = 'block';
+});
 
 // 탭메뉴 type2
 function tabType2(){
