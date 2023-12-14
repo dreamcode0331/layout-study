4f102e7a022383e40cef30e210465611701c2851
이미연 <dreamcode0331@gmail.com>
2023-12-14 18:33:36 +0900

    footer 영역 마크업 완료

--- knou/js/script.js
+++ knou/js/script.js
@@ -18,7 +18,6 @@ $(function(){
 
   $('.publish_list_slide').slick({
     infinite: true,
-    // fade: true,
     autoplay: true,
     autoplaySpeed: 2500,
     speed: 1000,
@@ -37,41 +36,28 @@ $(function(){
     prevArrow: $('.prev'),
     nextArrow: $('.next'),
   });
-  $('.local_univ_slide').on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){ 
-    var i = (currentSlide ? currentSlide : 0) + 1;
-    $('.slide_count').html("<span class='now'>" + i + "</span><span class='total'>" + slick.slideCount + "</span>");
+
+  $('.popup_slide').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    speed: 1000,
+    cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
   });
-
-  // $('.multiple_slide').slick({
-  //   infinite: true,
-  //   slidesToShow: 3,
-  //   lidesToScroll: 1,
-
-  //   responsive: [
-  //     {
-  //       breakpoint: 768,
-  //       settings: {
-  //         slidesToShow: 2,
-  //         slidesToScroll: 1,
-  //         infinite: true,
-  //         dots: true,
-  //         autoplay: true,
-  //         autoplaySpeed: 1500,
-  //       }
-  //     },
-  //   ]
-  //});
-
+  
   $('.pause').on('click', function(){
     $(this).hide();
     $('.play').show();
-    $('.single_slide, .publish_list_slide, .local_univ_slide').slick('slickPause');
+    $('.single_slide, .publish_list_slide, .local_univ_slide, popup_slide').slick('slickPause');
 
   });
+
   $('.play').on('click', function(){
     $(this).hide();
     $('.pause').show();
-    $('.single_slide, .publish_list_slide, .local_univ_slide').slick('slickPlay');
+    $('.single_slide, .publish_list_slide, .local_univ_slide, popup_slide').slick('slickPlay');
   });
 
   // slide
